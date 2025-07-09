2ee2ee7e92b8a4cbe490ea512c25985ffef7b33e
Alex Wilson <alex.dave999@gmail.com>
2025-07-09 13:59:51 +0100

    🚧 Rendering broken GLTF export

---
+++ src/views/Playground/NerdieHead/NerdieHead.vue
@@ -0,0 +1,10 @@
+<template>
+  <primitive :object="scene" />
+</template>
+
+<script setup lang="ts">
+import { useGLTF } from '@tresjs/cientos';
+import gltfPath from './alex_head_model.gltf?url';
+
+const { scene } = await useGLTF(gltfPath);
+</script>
--- src/views/Playground/TresView.vue
+++ src/views/Playground/TresView.vue
@@ -3,10 +3,31 @@
     <h1>Tres <RouterLink to="/playground"> Playground </RouterLink></h1>
   </header>
   <main>
-    <div>Test</div>
+    <div class="tres-canvas-container">
+      <TresCanvas clear-color="#82DBC5">
+        <TresPerspectiveCamera :position="[5, 5, 5]" />
+        <CameraControls make-default />
+        <Suspense>
+          <NerdieHead />
+        </Suspense>
+        <TresGridHelper :position="[0, -1, 0]" />
+        <TresAmbientLight />
+        <TresDirectionalLight :position="[0, 2, 4]" />
+      </TresCanvas>
+    </div>
   </main>
 </template>
 
 <script setup lang="ts">
 import { RouterLink } from 'vue-router';
+import { TresCanvas } from '@tresjs/core';
+import { CameraControls } from '@tresjs/cientos';
+import NerdieHead from './NerdieHead/NerdieHead.vue';
 </script>
+
+<style scoped lang="scss">
+.tres-canvas-container {
+  width: 65vw;
+  height: 80vh;
+}
+</style>
